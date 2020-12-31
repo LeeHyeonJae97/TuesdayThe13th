29db28c9b60081ba9fff126a13c90f0ffdb45680
현재 이 <qazx5024@gmail.com>
2020-12-31 11:58:39 +0900

    cleanup

--- Assets/Scripts/Portal/Portalteleport.cs
+++ Assets/Scripts/Portal/Portalteleport.cs
@@ -4,26 +4,26 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class portalteleport : MonoBehaviour
+public class Portalteleport : MonoBehaviour
 { 
     public Transform teleportTarget;
     public GameObject thePlayer;
 
     Collider m_objectCollider;
-    public void triggeron()
+    public void Triggeron()
     {
         m_objectCollider = GetComponent<Collider>();
         m_objectCollider.isTrigger = true;
     }
 
-    public void triggeroff()
+    public void Triggeroff()
     {
         m_objectCollider = GetComponent<Collider>();
         m_objectCollider.isTrigger = false;
     }
+
     private void OnTriggerStay(Collider other)
     {
         thePlayer.transform.position = teleportTarget.transform.position;
-    }
-    
+    }    
 }
