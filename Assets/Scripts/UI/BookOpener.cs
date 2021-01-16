61a66ba41ab01345147fedfd3f8d02fe26841675
leejeonghan <wjdgks300@naver.com>
2021-01-16 23:29:26 +0900

    book_menu

---
+++ Assets/Scripts/UI/BookOpener.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookOpener : MonoBehaviour
+{
+
+    public GameObject Panel;
+    // Start is called before the first frame update
+    public void OpenPanel()
+    {   
+        Panel.SetActive(true);
+    }
+
+    public void ClosePanel()
+    {
+        Panel.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
