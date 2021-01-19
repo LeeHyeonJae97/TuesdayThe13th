ba738020c90bb2cedc25aaa96a4eeb968a38677f
wjdgkswpdlf <wjdgkswpdlf@gmail.com>
2021-01-19 22:25:38 +0900

    bookj

---
+++ Assets/Scripts/Clue/ClueData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class ClueData 
+{
+    public string Cluename;
+    public string description;
+    public Transform Clueobject;
+    public Sprite ClueImage;
+}
---
+++ Assets/Scripts/Clue/Cluedatabase.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class Cluedatabase: MonoBehaviour
+{
+    public static Cluedatabase instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+    // Update is called once per frame
+    public List<ClueData>ClueDB = new List<ClueData>();
+}
